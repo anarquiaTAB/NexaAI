ac7bbe1697826d5dacce60c302674b0ecb504226
NexaAI Build <nexaai@build.com>
2026-07-23 01:58:36 +0000

    fix: adicionar import asPaddingValues faltando em ChatScreen.kt

--- app/src/main/java/com/nexaai/presentation/chat/ChatScreen.kt
+++ app/src/main/java/com/nexaai/presentation/chat/ChatScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
